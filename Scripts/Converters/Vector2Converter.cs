7b3e1949821b1187d4274d5773e8491b1de0c1df
Simon (Darkside) Jackson <darkside@zenithmoon.com>
2021-06-28 22:58:47 +0100

    Update Vector2Converter.cs
    Resolves Unity compilation issue with unused variables

--- Scripts/Converters/Vector2Converter.cs
+++ Scripts/Converters/Vector2Converter.cs
@@ -18,7 +18,7 @@ namespace Siccity.GLTFUtility.Converters {
 			float[] floatArray = null;
 			try {
 				floatArray = serializer.Deserialize<float[]>(reader);
-			} catch (System.Exception e) {
+			} catch {
 				floatArray = new float[] { serializer.Deserialize<float>(reader) };
 			}
 
